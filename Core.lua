dc4e2c5206abfe0dfcf55369df269cba0462e0e9
SergeyVaulin <SergeyVaulin@localhost>
2016-09-08 19:10:31 +0300

    55

--- Core.lua
+++ Core.lua
@@ -12,8 +12,6 @@ function PHFormat(message, markType)
 	end
 
 	local result = __merge("[PH]", message)
-
-	-- {РєСЂРµСЃС‚} [PH] Test {РєСЂРµСЃС‚}
 	return __merge(_L.Marks[mark], result, _L.Marks[mark])
 end
 
@@ -21,4 +19,32 @@ function PHSayInstance(message, markType)
 
 	-- Hook here
 	SendChatMessage(PHFormat(message, markType), "INSTANCE_CHAT" )
+end
+
+function __merge(str1, str2, str3)
+-- TODO на ... сделать
+	local result = ""
+	if str1 ~= nil then
+		result = __tostring(str1) 
+	end
+
+	if str2 ~= nil then
+		result = result .. " " .. __tostring(str2)
+	end
+	
+	if str3 ~= nil then
+		result = result .. " " .. __tostring(str3)
+	end
+
+	return result
+end
+
+-- Вывод параметров
+function __unpackToString(...)
+	local result = ""
+	local args = { params = select("#", ...), ... }
+	for i = 1, args.params do
+      result = result ..tostring(i)..") ".. tostring(args[i]) .. " | "
+	end
+	return result
 end
